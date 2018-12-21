64ca93b12c80db4b96e39672fe661348f6a71cb1
GNukala <GNukala@rsna.org>
2018-12-21 18:15:38 +0530

    excercise on custom block

---
+++ sites/default/files/php/twig/5c10b4c683dd1_block-content-add-list.ht_zlN2VdVYDjnOJQOslW6sIphve/KxSOI3uPmMFnSoq9gptrkohLn7HjRvuBbzjgzALisn8.php
@@ -0,0 +1,97 @@
+<?php
+
+/* core/themes/stable/templates/admin/block-content-add-list.html.twig */
+class __TwigTemplate_de1b2821cf5ee63ed0ddf4cc429d8b0b7b5c07081203a77a16903d04fabea5be extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("spaceless" => 15, "for" => 17);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('spaceless', 'for'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 15
+        ob_start();
+        // line 16
+        echo "  <dl>
+    ";
+        // line 17
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["types"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["type"]) {
+            // line 18
+            echo "      <dt>";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute($context["type"], "link", array()), "html", null, true));
+            echo "</dt>
+      <dd>";
+            // line 19
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute($context["type"], "description", array()), "html", null, true));
+            echo "</dd>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['type'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 21
+        echo "  </dl>
+";
+        echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/admin/block-content-add-list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  65 => 21,  57 => 19,  52 => 18,  48 => 17,  45 => 16,  43 => 15,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/themes/stable/templates/admin/block-content-add-list.html.twig", "D:\\xampp\\htdocs\\drupalsite\\core\\themes\\stable\\templates\\admin\\block-content-add-list.html.twig");
+    }
+}
